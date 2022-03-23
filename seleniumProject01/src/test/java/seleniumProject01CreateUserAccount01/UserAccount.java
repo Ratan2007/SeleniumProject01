29b8fae9bb8e8db54df80245c90870b3228da6f8
Ratan2007 <skratan2007@gmail.com>
2022-03-23 18:20:49 -0400

    Add TestNG

--- seleniumProject01/src/test/java/seleniumProject01CreateUserAccount01/UserAccount.java
+++ seleniumProject01/src/test/java/seleniumProject01CreateUserAccount01/UserAccount.java
@@ -2,13 +2,18 @@ package seleniumProject01CreateUserAccount01;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class UserAccount {
-	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\SKRAT\\eclipse-workspace02\\seleniumProject01\\src\\test\\resources\\Driver\\chromedriver.exe");
-	WebDriver driver=new ChromeDriver();
-driver.get("https://demo.nopcommerce.com/");
-	
+	@Test
+	public  void CreateUser() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\SKRAT\\git\\SeleniumProject01\\seleniumProject01\\src\\test\\resources\\Driver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.nopcommerce.com/");
+		
+		System.out.println("This is Md hafizur Rahman");
+		driver.close();
 	
 	
 	}
